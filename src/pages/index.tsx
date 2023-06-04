675d85e8946c7d017613c12f1b8c1cbf835bd52b
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-06-04 16:47:11 +0300

    Update index page to include necessary packages and redirect logged-in users

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,46 @@
-const Home = () => {
-  return <div style={{ height: "200vh" }}>Nothing here</div>;
+//  packages
+import Link from "next/link";
+import { useState } from "react";
+import { GetServerSidePropsContext } from "next";
+
+// server modules
+import { getTokenCookie } from "@lib/authCookies";
+
+// client modules & components
+import Login from "@/components/Login";
+import { redirect } from "@/utils/serverProps";
+
+const Landing = () => {
+  const [showLogin, setShowLogin] = useState(false);
+
+  return (
+    <div className="h-screen flex flex-col justify-center items-center gap-4">
+      <h1 className="text-4xl">Landing Page</h1>
+      <Link href="/home">Home page</Link>
+      <button onClick={() => setShowLogin(true)}>Login</button>
+      {showLogin && (
+        <>
+          <button
+            className="fixed top-8 left-8 text-4xl z-10"
+            onClick={() => setShowLogin(false)}
+          >
+            X
+          </button>
+          <Login />
+        </>
+      )}
+    </div>
+  );
 };
 
-export default Home;
+export const getServerSideProps = ({ req }: GetServerSidePropsContext) => {
+  const token = getTokenCookie(req);
+
+  if (token) return redirect("/home");
+
+  return {
+    props: {},
+  };
+};
+
+export default Landing;
