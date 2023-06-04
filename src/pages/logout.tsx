ee205b504ca5e52400cfcc699e496950e45fcc0b
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-06-04 16:41:56 +0300

    Add logout page to clear the cookie token and redirect users

---
+++ src/pages/logout.tsx
@@ -0,0 +1,19 @@
+import { ServerResponse } from "http";
+
+import { redirect } from "@/utils/serverProps";
+import { removeTokenCookie } from "@lib/authCookies";
+
+const Logout = () => {
+  return false;
+};
+
+export const getServerSideProps = ({ res }: { res: ServerResponse }) => {
+  try {
+    removeTokenCookie(res);
+    return redirect("/");
+  } catch (err) {
+    return redirect("/home");
+  }
+};
+
+export default Logout;
