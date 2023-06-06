d9dec2286b477522e4b6ff7082a67156092ff11d
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-06-06 18:20:12 +0300

    Refactor api handler to use http-proxy to forward request to an external API

---
+++ src/pages/api/[...path].ts
@@ -0,0 +1,68 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import httpProxy from "http-proxy";
+
+import { getTokenCookie } from "@lib/authCookies";
+import { API_URL } from "@/config";
+
+const target = API_URL;
+const proxy = httpProxy.createProxyServer({ target, autoRewrite: false });
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  return new Promise((resolve, reject) => {
+    const token = getTokenCookie(req);
+
+    // don't forwards the cookies to the target server
+    req.headers.cookie = "";
+
+    if (token) {
+      req.headers.authorization = `Bearer ${token}`;
+    }
+
+    proxy.once("error", reject);
+
+    proxy.web(req, res);
+  });
+};
+
+export default handler;
+
+/* Handling The request Manually without a proxy
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const method = req.method;
+
+  const token = getTokenCookie(req);
+
+  const options: AxiosRequestConfig = {
+    method,
+    headers: { Authorization: `Bearer ${token}` },
+  };
+
+  if (method === "POST" || method === "PUT") {
+    options.data = req.body;
+  }
+
+  try {
+    const response = await axios(`${target}${req.url}`, options);
+
+    res.status(response.status).json(response.data);
+  } catch (err) {
+    if (err instanceof AxiosError) {
+      res.status(err.response?.status ?? 500).json(err.response?.data);
+    } else {
+      res.status(500).json({
+        success: false,
+        data: null,
+        error: "Internal Server Error",
+      });
+    }
+  }
+};
+
+*/
