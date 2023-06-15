8067fa663688b058ae925d968f7b258e69c95e3d
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-06-15 21:26:59 +0300

    Remove handleBackdropClick middleware and return statement

--- src/components/Backdrop/index.tsx
+++ src/components/Backdrop/index.tsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import { MouseEvent, MouseEventHandler, ReactNode } from "react";
+import { MouseEventHandler, ReactNode } from "react";
 
 const opacityVariants = {
   hidden: { opacity: 0 },
@@ -11,24 +11,16 @@ interface Props {
   children: ReactNode;
 }
 
-const Backdrop = ({ onClick, children }: Props) => {
-  const handleBackdropClick = (e: MouseEvent<HTMLElement>) => {
-    e.stopPropagation();
-    onClick(e);
-  };
-
-  return (
-    <motion.div
-      variants={opacityVariants}
-      initial="hidden"
-      animate="visible"
-      exit="hidden"
-      onClick={handleBackdropClick}
-      className={`fixed inset-0 z-40 flex cursor-pointer items-center justify-center bg-accent/10 p-4 backdrop-blur-md`}
-    >
-      {children}
-    </motion.div>
-  );
-};
-
+const Backdrop = ({ onClick, children }: Props) => (
+  <motion.div
+    variants={opacityVariants}
+    initial="hidden"
+    animate="visible"
+    exit="hidden"
+    onClick={onClick}
+    className={`fixed inset-0 z-40 flex cursor-pointer items-center justify-center bg-accent/10 p-4 backdrop-blur-md`}
+  >
+    {children}
+  </motion.div>
+);
 export default Backdrop;
