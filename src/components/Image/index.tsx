81251a8811864fc665d5f1bb31d82b5e0b8b956b
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-06-15 09:50:51 +0300

    Add Image component to optimize image loading
    Adding a lazy load on image & display a skeleton loader while loading
    and the ability to reload the image when the loading failed

---
+++ src/components/Image/index.tsx
@@ -0,0 +1,73 @@
+import { StyleHTMLAttributes, useState } from "react";
+import { twMerge } from "tailwind-merge";
+
+import { TbFaceIdError } from "react-icons/tb";
+import { AiOutlineReload } from "react-icons/ai";
+
+import SkeletonLoader from "@/components/SkeletonLoader";
+
+interface Props {
+  src: string;
+  alt: string;
+  className?: string;
+  style?: StyleHTMLAttributes<HTMLImageElement>;
+}
+
+function Image({ src, alt, className = "", style }: Props) {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [reloadNum, setReloadNum] = useState(0);
+
+  const reload = () => {
+    setLoading(true);
+    setError(false);
+    setReloadNum(prevNum => prevNum + 1);
+  };
+
+  const handleError = () => {
+    setLoading(false);
+    setError(true);
+  };
+
+  // TODO: Add background-colot
+  return (
+    <picture
+      onClick={e => (error || loading) && reloadNum < 3 && e.stopPropagation()}
+      className={twMerge(
+        `block h-full overflow-hidden ${!loading ? "bg-" : ""} ${
+          error ? "cursor-auto" : ""
+        }`,
+        className
+      )}
+    >
+      {loading && <SkeletonLoader className={className} />}
+      {error && (
+        <div className="flex h-full w-full flex-col items-center justify-center gap-1">
+          <button
+            onClick={() => (reloadNum < 3 ? reload() : null)}
+            className="relative"
+          >
+            <TbFaceIdError className="text-[1.5em]" />
+            {reloadNum < 3 && (
+              <AiOutlineReload className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 scale-150 transform text-[2em]" />
+            )}
+          </button>
+        </div>
+      )}
+      <img
+        // To re-render img again
+        key={String(Math.random())}
+        src={src}
+        alt={alt}
+        style={style}
+        className={`h-full w-full bg-[var(--color)] object-cover ${
+          loading || error ? "hidden" : "block"
+        }`}
+        onLoad={() => setLoading(false)}
+        onError={handleError}
+      />
+    </picture>
+  );
+}
+
+export default Image;
