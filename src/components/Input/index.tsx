4b96d574c51d8f587dcbce1397a2174f1b9179a2
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-06-15 17:35:29 +0300

    Add styled Input component with updateValue onChange listener

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,55 @@
+import {
+  ChangeEvent,
+  ComponentProps,
+  Dispatch,
+  ReactNode,
+  SetStateAction,
+} from "react";
+import { twMerge } from "tailwind-merge";
+
+interface InputProps extends Partial<ComponentProps<"input">> {
+  // Value can be any type but for my inputs I just use strings
+  name: string;
+  value: string;
+  setValue: Dispatch<SetStateAction<{ [key: string]: string }>>;
+  label?: string;
+  labelClassName?: string;
+}
+
+const Input = ({
+  value,
+  setValue,
+  className,
+  label,
+  labelClassName,
+  ...props
+}: InputProps) => {
+  const updateValue = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+
+    setValue(prevValue => ({
+      ...prevValue,
+      [name]: value,
+    }));
+  };
+
+  return (
+    <label className={twMerge(`flex flex-col gap-2`, labelClassName)}>
+      {label}
+      <input
+        type="text"
+        {...props}
+        value={value}
+        className={twMerge(
+          `rounded-xl border-2 border-accent/20 bg-accent/10 p-3 caret-accent transition-colors focus:border-accent/40 focus:bg-transparent focus:outline-none ${
+            props.disabled ? "cursor-pointer" : ""
+          }`,
+          className
+        )}
+        onChange={updateValue}
+      />
+    </label>
+  );
+};
+
+export default Input;
