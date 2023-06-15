b26201ca265f677dc2734c26220338a2d499ec5a
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-06-15 21:05:30 +0300

    Add theme background, text, and accent colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,24 @@ module.exports = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/features/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      transitionDuration: {
+        DEFAULT: "250ms",
+      },
+      colors: {
+        tcolor: {
+          DEFAULT: "#e2e8f0",
+          2: "#94a3b8",
+        },
+        bcolor: {
+          DEFAULT: "#131313",
+          2: "#2e333d",
+        },
+        accent: "#6b8afd",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
