c6c5b8c02e46d1ce89cee652df691d3828d7c6cd
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-06-15 21:27:58 +0300

    Add config file to return SERVER_URL and API_URL based on node env

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,14 @@
 const dev = process.env.NODE_ENV !== "production";
 
-// TODO: change the production url when deploy the project
-export const server = dev
-  ? "http://localhost:3000"
-  : "https://productionhost.com";
+const DEV_SERVER = process.env.DEV_SERVER;
+const PROD_SERVER = process.env.PROD_SERVER;
+
+const DEV_API_URL = process.env.DEV_API_URL;
+const PROD_API_URL = process.env.PROD_API_URL;
+
+if (dev && (!DEV_SERVER || !DEV_API_URL)) {
+  throw new Error("Invalid/Missing URL variables in .env.local");
+}
+
+export const SERVER_URL = (dev ? DEV_SERVER : PROD_SERVER) as string;
+export const API_URL = (dev ? DEV_API_URL : PROD_API_URL) as string;
