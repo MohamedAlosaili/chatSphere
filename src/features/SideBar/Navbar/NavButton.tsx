8cb70bd79d9c0ffaf3ae0401ba6381920ebc4d8d
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-06-15 21:32:42 +0300

    Style reformat TW classes with prettier plugin

--- src/features/SideBar/Navbar/NavButton.tsx
+++ src/features/SideBar/Navbar/NavButton.tsx
@@ -6,18 +6,18 @@ type Props = ComponentProps<"button"> & { active: boolean };
 const NavButton = ({ children, className, active, ...props }: Props) => (
   <button
     className={twMerge(
-      `relative transition-colors p-4 hover:text-tcolor lg:w-full ${
-        active ? "text-tcolor bg-accent/10" : ""
+      `relative p-4 transition-colors hover:text-tcolor lg:w-full ${
+        active ? "bg-accent/10 text-tcolor" : ""
       }`,
       className
     )}
     {...props}
   >
-    <div className="lg:w-fit lg:mx-auto">{children}</div>
+    <div className="lg:mx-auto lg:w-fit">{children}</div>
     <div
-      className={`transition-colors absolute bottom-0 left-1/2 lg:top-1/2 lg:-translate-y-1/2 lg:left-0 -translate-x-1/2 h-2 lg:h-3/4 ${
+      className={`absolute bottom-0 left-1/2 h-2 -translate-x-1/2 transition-colors lg:left-0 lg:top-1/2 lg:h-3/4 lg:-translate-y-1/2 ${
         active ? "bg-accent" : "bg-transparent"
-      } w-3/4 lg:w-2 rounded-tl-md rounded-tr-md`}
+      } w-3/4 rounded-tl-md rounded-tr-md lg:w-2`}
     ></div>
   </button>
 );
