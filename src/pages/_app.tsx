ff58927470a57470159e4eac44ee0d0e9c866e6a
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-06-15 21:10:54 +0300

    Add description, keywords, and title meta tags to improve SEO

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,45 @@
-import type { AppProps } from "next/app";
 import Head from "next/head";
+import type { AppProps } from "next/app";
 
-import Layout from "@/components/Layout";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
+    <>
       <Head>
         <title>ChatSphere 💬</title>
-        {/* TODO: Add meta tags for improving SEO - alt: next-seo */}
+        <meta
+          name="description"
+          content="ChatSphere is a real-time web chat application that allows users to connect, chat, and communicate in real-time. Join the conversation and connect with people from around the world."
+        />
+        <meta
+          name="keywords"
+          content="chat application, web chat, real-time chat, chat platform, messaging app, online communication, chat with friends, connect with people"
+        />
+        <meta
+          property="og:title"
+          content="ChatSphere - Real-Time Web Chat Application"
+        />
+        <meta
+          property="og:description"
+          content="ChatSphere is a real-time web chat application that allows users to connect, chat, and communicate in real-time. Join the conversation and connect with people from around the world."
+        />
+        {/* TODO: Add image */}
+        <meta property="og:image" content="" />
+        <meta
+          name="twitter:title"
+          content="ChatSphere - Real-Time Web Chat Application"
+        />
+        <meta name="twitter:card" content="summary" />
+        <meta
+          name="twitter:description"
+          content="ChatSphere is a real-time web chat application that allows users to connect, chat, and communicate in real-time. Join the conversation and connect with people from around the world."
+        />
+        {/* TODO: Add image */}
+        <meta property="twitter:image" content="" />
       </Head>
       <Component {...pageProps} />
-    </Layout>
+      <div id="modal"></div>
+    </>
   );
 }
