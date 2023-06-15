7eedd89ab81a4942f2cac9894ff787c851adea01
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-06-15 17:41:09 +0300

    Add reusable Button component with alt option and animation

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,24 @@
+import { twMerge } from "tailwind-merge";
+import { motion, HTMLMotionProps } from "framer-motion";
+
+interface Props extends HTMLMotionProps<"button"> {
+  alt?: boolean; // alternative button style
+}
+
+const Button = ({ className, alt = false, ...props }: Props) => (
+  <motion.button
+    whileHover={{ scale: 1.1 }}
+    whileTap={{ scale: 0.95 }}
+    className={twMerge(
+      `mx-auto flex min-h-[2.5rem] items-center justify-center gap-2 rounded-xl px-6 py-2 text-sm font-medium text-white transition-colors ${
+        alt
+          ? "border-2 border-accent bg-transparent hover:bg-accent/25"
+          : "bg-accent/75 hover:bg-accent/50"
+      }`,
+      className
+    )}
+    {...props}
+  />
+);
+
+export default Button;
