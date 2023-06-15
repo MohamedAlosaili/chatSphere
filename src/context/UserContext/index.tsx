71bd2dc1552a8f7e59bb0a9cebdfbab2439379d1
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-06-15 21:19:34 +0300

    Add UserContext to share currentUser info across components

---
+++ src/context/UserContext/index.tsx
@@ -0,0 +1,49 @@
+import React, { ReactNode, useContext, useState } from "react";
+
+import useUserStatus from "./useUserStatus";
+import { fetcher } from "@/lib/fetcher";
+import { toast } from "react-toastify";
+
+import { TUser } from "@/types";
+
+interface TUserContext {
+  user: TUser | null;
+  updateCurrentUser: () => Promise<void>;
+}
+
+const UserContext = React.createContext<TUserContext>({
+  user: null,
+  updateCurrentUser: async () => {},
+});
+
+interface Props {
+  user: TUser;
+  children: ReactNode;
+}
+
+const UserContextProvider = (props: Props) => {
+  const [user, setUser] = useState<TUser>({ ...props.user });
+  useUserStatus(user, setUser);
+  if (!user) {
+    return <div>Loading...</div>;
+  }
+
+  const updateCurrentUser = async () => {
+    const result = await fetcher<TUser>("/api/auth/currentUser");
+    if (result.success && result.data) {
+      setUser(result.data);
+    } else {
+      toast.error("Something went wrong while updating user info!");
+    }
+  };
+
+  return (
+    <UserContext.Provider value={{ user, updateCurrentUser }}>
+      {props.children}
+    </UserContext.Provider>
+  );
+};
+
+export const useUserContext = () => useContext(UserContext);
+
+export default UserContextProvider;
