71735610a47e4ccae1a448c64f86718dc9ffcb14
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-06-16 17:06:04 +0300

    Add unreadMessages hook to retrieve the number of unread messages of each room

---
+++ src/features/SideBar/MyRooms/RoomCard/useUnreadMessages.ts
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+
+import { fetcher } from "@/lib/fetcher";
+
+const useUnreadMessages = (roomId: string) => {
+  const [unreadMessages, setUnreadMessages] = useState(0);
+  useEffect(() => {
+    fetcher(`/api/rooms/${roomId}/messages/unread`).then(res => {
+      setUnreadMessages(res.data);
+    });
+  }, []);
+
+  return unreadMessages;
+};
+
+export default useUnreadMessages;
