c76232376319e828af588f9b2ccc0d1a2a702e93
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-06-16 17:46:59 +0300

    Add relativeTime utility to convert date to human-readable

---
+++ src/features/SideBar/MyRooms/RoomCard/getRelativeTime.ts
@@ -0,0 +1,23 @@
+const weekDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+const relativeTime = (date: string) => {
+  const originalTime = new Date(date);
+
+  const originalTimeInSeconds = Math.floor(originalTime.getTime() / 1000);
+  const now = Math.floor(Date.now() / 1000);
+
+  const day = 24 * 60 * 60;
+  const week = 7 * day;
+
+  const difference = now - originalTimeInSeconds;
+
+  if (difference < day) {
+    return originalTime.toLocaleTimeString(undefined, { timeStyle: "short" });
+  } else if (difference < week) {
+    return weekDays[originalTime.getDay()];
+  } else {
+    return originalTime.toLocaleDateString();
+  }
+};
+
+export default relativeTime;
