30bb901c70e54aef52ebef0cb9f958d9c3e896fb
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-06-17 01:46:25 +0300

    Update Modal to be scrollable if it reaches the viewport edges

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -5,6 +5,7 @@ import { twMerge } from "tailwind-merge";
 
 import Backdrop from "../Backdrop";
 import Button from "../Button";
+import Scrollable from "../Scrollable";
 
 const modalVariants = {
   hidden: {
@@ -50,32 +51,36 @@ const Modal = ({
         exit="hidden"
         transition={{ y: { duration: 0.25, type: "Tween" } }}
         className={twMerge(
-          `flex w-96 max-w-full cursor-auto flex-col gap-4 rounded-2xl bg-bcolor p-6 text-sm text-tcolor`,
+          `relative flex max-h-full w-96 max-w-full cursor-auto flex-col overflow-hidden rounded-2xl bg-bcolor text-sm text-tcolor`,
           className
         )}
         onClick={e => e.stopPropagation()}
       >
-        {title?.text && (
-          <h1 className="flex items-center justify-center gap-2 text-xl font-semibold">
-            {title.text} {title?.icon ?? ""}
-          </h1>
-        )}
-        {children}
-        <div className="flex gap-4">
-          <Button
-            onClick={() => (loading ? null : close())}
-            className="flex-1"
-            alt={true}
-          >
-            Cancel
-          </Button>
-          <Button
-            onClick={e => (loading ? null : actionHandler(e))}
-            className="flex-1"
-          >
-            {actionName}
-          </Button>
-        </div>
+        <Scrollable className="flex flex-col gap-4 p-6">
+          {title?.text && (
+            <h1 className="flex items-center justify-center gap-2 text-xl font-semibold">
+              {title.text} {title?.icon ?? ""}
+            </h1>
+          )}
+          {children}
+          <div className="flex gap-4">
+            <Button
+              onClick={() => (loading ? null : close())}
+              className="flex-1"
+              alt={true}
+            >
+              Cancel
+            </Button>
+            <Button
+              onClick={e => (loading ? null : actionHandler(e))}
+              className="flex-1"
+            >
+              {actionName}
+            </Button>
+          </div>
+          <div className="absolute bottom-0 left-0 h-6 w-full bg-[linear-gradient(to_top,_rgb(19_19_19),_transparent)]"></div>
+          <div className="absolute left-0 top-0 h-6 w-full bg-[linear-gradient(to_bottom,_rgb(19_19_19),_transparent)]"></div>
+        </Scrollable>
       </motion.div>
     </Backdrop>,
     document.getElementById("modal") as HTMLElement
