f76878d7b8e4379ae69d02ffb7dce71d0e0128cf
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-06-16 13:00:57 +0300

    Remove extra div

--- src/components/Scrollable/index.tsx
+++ src/components/Scrollable/index.tsx
@@ -1,10 +1,14 @@
 import { ComponentProps } from "react";
 import scrollStyle from "./scrollable.module.scss";
 
-const Scrollable = (props: ComponentProps<"div">) => {
+const Scrollable = ({
+  className,
+  children,
+  ...props
+}: ComponentProps<"div">) => {
   return (
-    <div {...props}>
-      <div className={scrollStyle.scrollable}>{props.children}</div>
+    <div {...props} className={scrollStyle.scrollable + " " + className}>
+      {children}
     </div>
   );
 };
