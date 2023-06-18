9fd57989aa60875d3baeab62b86f2659ae9d385a
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-06-18 17:45:29 +0300

    Add disabled button style

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -7,14 +7,16 @@ interface Props extends HTMLMotionProps<"button"> {
 
 const Button = ({ className, alt = false, ...props }: Props) => (
   <motion.button
-    whileHover={{ scale: 1.1 }}
-    whileTap={{ scale: 0.95 }}
+    whileHover={props.disabled ? {} : { scale: 1.1 }}
+    whileTap={props.disabled ? {} : { scale: 0.95 }}
     className={twMerge(
-      `mx-auto flex min-h-[2.5rem] items-center justify-center gap-2 rounded-xl px-6 py-2 text-sm font-medium text-white transition-colors ${
-        alt
-          ? "border-2 border-accent bg-transparent hover:bg-accent/25"
-          : "bg-accent/75 hover:bg-accent/50"
-      }`,
+      `mx-auto flex min-h-[2.5rem] items-center justify-center gap-2 rounded-xl px-6 py-2 text-sm font-medium text-white transition-colors bg-accent/75`,
+      alt && "border-2 border-accent bg-transparent",
+      props.disabled
+        ? "opacity-50 transition-opacity"
+        : alt
+        ? "hover:bg-accent/25"
+        : "hover:bg-accent/50",
       className
     )}
     {...props}
