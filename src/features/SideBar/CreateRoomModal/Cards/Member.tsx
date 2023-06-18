c451108176f95afba4a9117d81a8abd278fd2b4f
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-06-18 17:41:47 +0300

    Update updateMembers method to required

--- src/features/SideBar/CreateRoomModal/Cards/Member.tsx
+++ src/features/SideBar/CreateRoomModal/Cards/Member.tsx
@@ -2,7 +2,7 @@ import { TUser } from "@/types";
 
 interface MemberProps {
   member: TUser;
-  updateMembers?: (member: TUser) => void;
+  updateMembers: (member: TUser) => void;
 }
 
 const Member = ({ member, updateMembers }: MemberProps) => (
@@ -10,7 +10,7 @@ const Member = ({ member, updateMembers }: MemberProps) => (
     {member.username}
     <div
       className="absolute inset-0 bg-bcolor opacity-0 transition-opacity group-hover:opacity-100"
-      onClick={() => updateMembers && updateMembers(member)}
+      onClick={() => updateMembers(member)}
     >
       <span className="grid h-full w-full place-items-center bg-accent/40 text-xl">
         ×
