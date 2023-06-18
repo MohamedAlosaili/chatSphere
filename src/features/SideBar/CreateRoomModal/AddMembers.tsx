17e0f53fda7844f5c988b253b9caae2ef49353c9
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-06-18 11:50:46 +0300

    Add AddMembers tap in CreateRoomModal to select room's members

---
+++ src/features/SideBar/CreateRoomModal/AddMembers.tsx
@@ -0,0 +1,113 @@
+import { useState } from "react";
+
+import useDocuments from "@/hooks/useDocuments";
+import useSearch from "@/hooks/useSearch";
+import Scrollable from "@/components/Scrollable";
+import Button from "@/components/Button";
+import Search from "@/components/Search";
+import { Member, User } from "./Cards";
+
+// Types
+import { Dispatch, SetStateAction } from "react";
+import { TUser } from "@/types";
+
+interface AddMembersProps {
+  members: TUser[];
+  setMembers: Dispatch<SetStateAction<TUser[]>>;
+}
+
+const AddMembers = ({ members, setMembers }: AddMembersProps) => {
+  const [users, usersLoading, update, total] =
+    useDocuments<TUser>("/api/users/online");
+
+  const {
+    result,
+    nextPage,
+    submittedValue,
+    total: totalSearch,
+    ...search
+  } = useSearch<TUser>("users");
+
+  const [showSearch, setShowSearch] = useState(false);
+  const showUsersFromSearch = showSearch && submittedValue;
+
+  const usersToDisplay = showUsersFromSearch ? result : users;
+  const totalUsers = showUsersFromSearch ? totalSearch : total;
+  const more = () => (showUsersFromSearch ? nextPage() : update(true));
+
+  const moreListToLoad =
+    usersToDisplay.length > 0 && totalUsers > usersToDisplay?.length;
+
+  const updateMembers = (newMember: TUser) => {
+    const isExist = members.some(member => member._id === newMember._id);
+
+    if (isExist) {
+      setMembers(prevMembers =>
+        prevMembers.filter(member => member._id !== newMember._id)
+      );
+    } else {
+      setMembers(prevMembers => [...prevMembers, newMember]);
+    }
+  };
+
+  return (
+    <>
+      <h3 className="font-medium">
+        Members <span>{`(${members.length})`}</span>{" "}
+      </h3>
+      <div className="flex flex-wrap gap-1">
+        {members?.map(member => (
+          <Member
+            key={member._id}
+            member={member}
+            updateMembers={updateMembers}
+          />
+        ))}
+      </div>
+
+      <h3 className="flex items-center font-medium">
+        Select from online users{" "}
+        <Button
+          className="mx-0 ml-auto min-h-[auto] rounded-lg px-2 py-1 text-xs"
+          onClick={() => setShowSearch(prev => !prev)}
+        >
+          {showSearch ? "Hide Search" : "Show Search"}
+        </Button>
+      </h3>
+      {showSearch && <Search placeholder="Search in users..." {...search} />}
+
+      <div className="h-40 rounded-xl border-2 border-accent/20 p-2">
+        <Scrollable>
+          {usersToDisplay.map(user => (
+            <User
+              key={user._id}
+              user={user}
+              members={members}
+              updateMembers={updateMembers}
+            />
+          ))}
+          {moreListToLoad && (
+            <Button onClick={more} className="min-h-fit w-3/4">
+              {usersLoading ? "Loading..." : "More"}
+            </Button>
+          )}
+          {usersToDisplay.length === 0 &&
+            (showUsersFromSearch ? (
+              <p className="text-tcolor text-center mt-4">
+                No results for{" "}
+                <span className="bg-accent/5 italic text-tcolor-2">
+                  {submittedValue}
+                </span>
+              </p>
+            ) : (
+              <p className="text-tcolor text-center mt-4">
+                No one is online now. Try searching
+              </p>
+            ))}
+        </Scrollable>
+      </div>
+    </>
+  );
+};
+
+export default AddMembers;
