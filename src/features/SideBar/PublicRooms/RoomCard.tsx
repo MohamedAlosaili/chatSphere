e0ce3a198212d95fb631662133cdbd0e53034888
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-06-19 00:44:34 +0300

    Add PublicRooms feature to list all available rooms to join

---
+++ src/features/SideBar/PublicRooms/RoomCard.tsx
@@ -0,0 +1,70 @@
+import { useState } from "react";
+import { toast } from "react-toastify";
+import { CgSpinner } from "react-icons/cg";
+
+import { useRoomContext } from "@/context/RoomContext";
+import Card from "@/components/Card";
+import Button from "@/components/Button";
+import Image from "@/components/Image";
+import getRoomPhoto from "@/utils/getPhoto";
+import { fetcher } from "@/lib/fetcher";
+
+// Types
+import { Dispatch, SetStateAction } from "react";
+import { TRoom } from "@/types";
+import { Tap } from "../types";
+
+interface RoomCardProps {
+  setActiveTap: Dispatch<SetStateAction<Tap>>;
+  room: TRoom;
+}
+
+const RoomCard = ({ room, setActiveTap }: RoomCardProps) => {
+  const { changeRoom } = useRoomContext();
+  const [loading, setLoading] = useState(false);
+
+  const joinRoom = async () => {
+    setLoading(true);
+
+    const res = await fetcher(`/api/rooms/${room._id}/members/join`, {
+      method: "POST",
+    });
+
+    setLoading(false);
+
+    if (res.success) {
+      toast.success(res.message ?? "Joined");
+      changeRoom(room);
+      setActiveTap("my rooms");
+    } else {
+      toast.error("Failed to join this room");
+    }
+  };
+
+  return (
+    <Card className="cursor-auto items-center">
+      <Image
+        src={getRoomPhoto(room.photo)}
+        alt={`${room.name} photo`}
+        className="aspect-square w-14 rounded-xl"
+      />
+      <h3 className="truncate font-medium text-tcolor">{room.name}</h3>
+
+      <Button
+        disabled={loading}
+        className={loading ? "opacity-100" : ""}
+        onClick={joinRoom}
+      >
+        <div className="min-w-[2rem]">
+          {loading ? (
+            <CgSpinner className="mx-auto animate-[spin_0.5s_linear_infinite;] text-2xl" />
+          ) : (
+            "Join"
+          )}
+        </div>
+      </Button>
+    </Card>
+  );
+};
+
+export default RoomCard;
---
+++ src/features/SideBar/PublicRooms/index.tsx
@@ -0,0 +1,92 @@
+import useDocuments from "@/hooks/useDocuments";
+import Button from "@/components/Button";
+import RoomCard from "./RoomCard";
+import Search, { useSearch, NoResult } from "@/features/Search";
+import { CardsSkeleton } from "@/components/Card";
+
+// Types
+import { Dispatch, SetStateAction } from "react";
+import { ToggleModalOptions, Tap } from "../types";
+import { TRoom } from "@/types";
+
+interface PublicRoomsProps {
+  toggleCreateRoomModal: (options?: ToggleModalOptions) => void;
+  setActiveTap: Dispatch<SetStateAction<Tap>>;
+}
+
+const PublicRooms = ({
+  toggleCreateRoomModal,
+  setActiveTap,
+}: PublicRoomsProps) => {
+  const [rooms, loading, update, total] = useDocuments<TRoom>("/api/rooms");
+  const [[result, searchValue, nextPage, totalSearch], search] =
+    useSearch<TRoom>("rooms");
+
+  return (
+    <div>
+      <Search
+        placeholder="Search in my rooms..."
+        results={result.length}
+        {...search}
+      />
+      {searchValue ? (
+        search.loading && result.length === 0 ? (
+          <CardsSkeleton size={5} />
+        ) : (
+          <>
+            {result.length > 0 ? (
+              <>
+                {result.map(room => (
+                  <RoomCard
+                    key={room._id}
+                    room={room}
+                    setActiveTap={setActiveTap}
+                  />
+                ))}
+                {totalSearch > result.length && (
+                  <Button onClick={nextPage} className="w-3/4">
+                    {search.loading ? "Loading..." : "More"}
+                  </Button>
+                )}
+              </>
+            ) : (
+              <NoResult value={searchValue} className="mt-8" />
+            )}
+          </>
+        )
+      ) : (
+        <div className="pt-4">
+          {loading && rooms.length === 0 ? (
+            <CardsSkeleton size={5} />
+          ) : rooms.length > 0 ? (
+            <>
+              {rooms.map(room => (
+                <RoomCard
+                  key={room._id}
+                  room={room}
+                  setActiveTap={setActiveTap}
+                />
+              ))}
+              {total > rooms.length && (
+                <Button onClick={() => update(true)} className="w-3/4">
+                  {loading ? "Loading..." : "More"}
+                </Button>
+              )}
+            </>
+          ) : (
+            <>
+              <h3 className="font-lg mb-4 mt-8 flex items-center justify-center gap-4 text-tcolor">
+                There are no public rooms available to join
+              </h3>
+              <Button onClick={() => toggleCreateRoomModal()}>
+                Create your own room
+              </Button>
+            </>
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default PublicRooms;
