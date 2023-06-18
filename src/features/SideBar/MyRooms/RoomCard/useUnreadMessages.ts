48af4e2eb7fbff801047c55c660913a91e389e5a
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-06-19 00:36:09 +0300

    Update hook to match the shape of unreadMessages response

--- src/features/SideBar/MyRooms/RoomCard/useUnreadMessages.ts
+++ src/features/SideBar/MyRooms/RoomCard/useUnreadMessages.ts
@@ -6,7 +6,7 @@ const useUnreadMessages = (roomId: string) => {
   const [unreadMessages, setUnreadMessages] = useState(0);
   useEffect(() => {
     fetcher(`/api/rooms/${roomId}/messages/unread`).then(res => {
-      setUnreadMessages(res.data);
+      setUnreadMessages(res.data?.unreadMessages ?? 0);
     });
   }, []);
 
