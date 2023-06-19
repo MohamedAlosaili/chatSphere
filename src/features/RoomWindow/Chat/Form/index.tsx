83000faad847a474d61e573235bf02fcdd71184d
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-06-19 18:54:29 +0300

    Add Form component for sending messages as file or plain text

---
+++ src/features/RoomWindow/Chat/Form/index.tsx
@@ -0,0 +1,96 @@
+import { useState, useRef, useEffect, FormEvent, LegacyRef } from "react";
+import { AnimatePresence } from "framer-motion";
+import { TbSquareRoundedPlusFilled } from "react-icons/tb";
+import { FiSend } from "react-icons/fi";
+import { toast } from "react-toastify";
+
+import { useRoomContext } from "@/context/RoomContext";
+import Input from "@/components/Input";
+import InputFile from "@/components/InputFile";
+import Button from "@/components/Button";
+import Preview from "./Preview";
+import { fetcher } from "@/lib/fetcher";
+import { socket } from "@/lib/socket";
+
+// Types
+import { IndexSignature } from "@/types";
+
+interface Props {
+  updateMessages: (nextPage?: boolean) => void;
+}
+
+const Form = ({ updateMessages }: Props) => {
+  const { activeRoom } = useRoomContext();
+  const [file, setFile] = useState<File>();
+  const [message, setMessage] = useState<IndexSignature<string>>({ text: "" });
+  const [loading, setLoading] = useState(false);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (message.text === "") inputRef.current?.focus();
+  }, [message.text]);
+
+  const sendMessage = async (e: FormEvent) => {
+    e.preventDefault();
+    const formData = new FormData();
+
+    if (file) {
+      formData.append("file", file);
+    }
+
+    formData.append("content", message.text.trim());
+    setLoading(true);
+    const res = await fetcher(`/api/rooms/${activeRoom?._id}/messages`, {
+      method: "POST",
+      data: formData,
+    });
+    setLoading(false);
+
+    if (res.success) {
+      setMessage({ text: "" });
+      setFile(undefined);
+      updateMessages();
+      socket.emit("new message", activeRoom?._id);
+    } else {
+      return toast.error("Failed to send a message, try again");
+    }
+  };
+
+  return (
+    <div className="sticky bottom-0 left-0 z-10 h-20 w-full shrink-0 border-t border-accent/25 bg-bcolor-2 p-4 text-tcolor">
+      <form onSubmit={sendMessage} className="relative mx-auto lg:max-w-[75%]">
+        <Input
+          inputRef={inputRef}
+          name="text"
+          value={message.text}
+          setValue={setMessage}
+          disabled={loading}
+          placeholder="Type a message..."
+          className="pr-24"
+        />
+        <div className="absolute right-0 top-1/2 flex -translate-y-1/2 items-center gap-2 pr-3">
+          <Button type="button" className="min-h-fit p-0">
+            <InputFile
+              accept="image, video"
+              setFile={setFile}
+              disabled={loading}
+            >
+              <TbSquareRoundedPlusFilled className="text-3xl text-tcolor" />
+            </InputFile>
+          </Button>{" "}
+          <Button
+            className="aspect-square min-h-fit w-8 p-0"
+            disabled={(!message.text.trim() && !file) || loading}
+          >
+            <FiSend className="text-lg" />
+          </Button>
+        </div>
+        <AnimatePresence>
+          {file && <Preview file={file} setFile={setFile} loading={loading} />}
+        </AnimatePresence>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
