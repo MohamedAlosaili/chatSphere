5d9264114249b0a36d08611881a9f022ace40496
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-06-20 02:58:28 +0300

    Update socket setup to use NEXT_PUBLIC_* variables

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -2,9 +2,11 @@ import { io } from "socket.io-client";
 
 const URL =
   process.env.NODE_ENV === "production"
-    ? process.env.PROD_SOCKET_URL
-    : process.env.DEV_SOCKET_URL;
+    ? process.env.NEXT_PUBLIC_PROD_SOCKET_URL
+    : process.env.NEXT_PUBLIC_DEV_SOCKET_URL;
 
-if (!URL) throw new Error("Missing SOCKET_URL in the environment variables");
+if (!URL) {
+  throw new Error("Missing SOCKET_URL in the environment variables");
+}
 
-export const socket = io(URL);
+export const socket = io("http://localhost:5000");
