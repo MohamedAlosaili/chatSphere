65375ef7e8a35fa105779dfd82aac2789900d5fc
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-06-19 22:17:54 +0300

    Update to run on every rerender to update unread messages number

--- src/features/SideBar/MyRooms/RoomCard/useUnreadMessages.ts
+++ src/features/SideBar/MyRooms/RoomCard/useUnreadMessages.ts
@@ -2,13 +2,15 @@ import { useEffect, useState } from "react";
 
 import { fetcher } from "@/lib/fetcher";
 
-const useUnreadMessages = (roomId: string) => {
+const useUnreadMessages = (roomId: string, key: string) => {
   const [unreadMessages, setUnreadMessages] = useState(0);
+
+  // TODO: update useEffect here - find a better way
   useEffect(() => {
     fetcher(`/api/rooms/${roomId}/messages/unread`).then(res => {
       setUnreadMessages(res.data?.unreadMessages ?? 0);
     });
-  }, []);
+  }, [key]);
 
   return unreadMessages;
 };
