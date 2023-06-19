a9d21a9062c699646630c0289e95475abe70ba5c
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-06-19 20:37:48 +0300

    Update lazy load Chat component

--- src/features/RoomWindow/index.tsx
+++ src/features/RoomWindow/index.tsx
@@ -1,15 +1,18 @@
-import { AnimatePresence } from "framer-motion";
-
-import Chat from "./Chat";
+import { lazy, Suspense } from "react";
 import { useRoomContext } from "@/context/RoomContext";
+
+const Chat = lazy(() => import("./Chat"));
 
 const RoomWindow = () => {
   const { activeRoom } = useRoomContext();
 
   return (
     <div className="flex items-center justify-center bg-bcolor-2">
+      {/* TODO: add fallback loader */}
       {activeRoom ? (
-        <Chat key={activeRoom._id} />
+        <Suspense fallback={""}>
+          <Chat key={activeRoom._id} />
+        </Suspense>
       ) : (
         <p className="m-2 rounded-xl bg-accent/20 px-3 py-2 text-tcolor">
           Select a room to start messaging...
