7bc5a99b690326beaed2dc39841be90e3fb523df
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-06-22 13:09:47 +0300

    Fix & Update invalid condition

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,7 +3,7 @@ const dev = process.env.NODE_ENV !== "production";
 const DEV_API_URL = process.env.DEV_API_URL;
 const PROD_API_URL = process.env.PROD_API_URL;
 
-if (!DEV_API_URL || PROD_API_URL) {
+if ((dev && !DEV_API_URL) || (!dev && !PROD_API_URL)) {
   throw new Error("Invalid/Missing API_URL variable in .env");
 }
 
