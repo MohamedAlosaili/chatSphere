ee135d1c1ebf73b654ff22fba636a08824184d7a
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-06-22 13:00:34 +0300

    Update config to ensure API_URL exists & Remove unused SERVER_URL

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,14 +1,10 @@
 const dev = process.env.NODE_ENV !== "production";
 
-const DEV_SERVER = process.env.DEV_SERVER;
-const PROD_SERVER = process.env.PROD_SERVER;
-
 const DEV_API_URL = process.env.DEV_API_URL;
 const PROD_API_URL = process.env.PROD_API_URL;
 
-if (dev && (!DEV_SERVER || !DEV_API_URL)) {
-  throw new Error("Invalid/Missing URL variables in .env.local");
+if (!DEV_API_URL || PROD_API_URL) {
+  throw new Error("Invalid/Missing API_URL variable in .env");
 }
 
-export const SERVER_URL = (dev ? DEV_SERVER : PROD_SERVER) as string;
 export const API_URL = (dev ? DEV_API_URL : PROD_API_URL) as string;
