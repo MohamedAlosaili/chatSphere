d8c5499eaf0793ff833392c856841a6a0b804c37
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-06-23 14:12:34 +0300

    Add animation when a chat opened

--- src/features/RoomWindow/index.tsx
+++ src/features/RoomWindow/index.tsx
@@ -1,23 +1,37 @@
-import { lazy, Suspense } from "react";
+import { lazy, Suspense, ComponentType } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+
 import { useRoomContext } from "@/context/RoomContext";
 
 const Chat = lazy(() => import("./Chat"));
+
+// const delay = (file: any) => {
+//   return new Promise((res, rej) =>
+//     setTimeout(() => res(file), 5000)
+//   ) as Promise<{ default: ComponentType<any> }>;
+// };
 
 const RoomWindow = () => {
   const { activeRoom } = useRoomContext();
 
   return (
-    <div className="flex items-center justify-center bg-bcolor-2">
+    <div className="absolute left-full top-0 flex items-center justify-center bg-bcolor-2 md:static">
       {/* TODO: add fallback loader */}
-      {activeRoom ? (
-        <Suspense fallback={""}>
-          <Chat key={activeRoom._id} />
-        </Suspense>
-      ) : (
-        <p className="m-2 rounded-xl bg-accent/20 px-3 py-2 text-tcolor">
-          Select a room to start messaging...
-        </p>
-      )}
+      <Suspense fallback={"loading..."}>
+        <AnimatePresence mode="wait">
+          {activeRoom ? (
+            <Chat key={activeRoom._id} />
+          ) : (
+            <motion.p
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              className="m-2 rounded-xl bg-accent/20 px-3 py-2 text-tcolor"
+            >
+              Select a room to start messaging...
+            </motion.p>
+          )}
+        </AnimatePresence>
+      </Suspense>
     </div>
   );
 };
