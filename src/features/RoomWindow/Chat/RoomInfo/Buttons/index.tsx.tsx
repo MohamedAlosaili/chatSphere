a93e62f59faae0a4b769d21447ad7d909a96fe34
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-06-24 21:42:19 +0300

    Add Buttons component for updating and leaving rooms

---
+++ src/features/RoomWindow/Chat/RoomInfo/Buttons/index.tsx.tsx
@@ -0,0 +1,34 @@
+import useLeaveRoom from "./useLeaveRoom";
+import Button from "@/components/Button";
+
+// Types
+import { TRoom } from "@/types";
+
+interface ButtonsProps {
+  room: TRoom;
+  currentUserIsRoomOwner: boolean;
+}
+
+const Buttons = ({ room, currentUserIsRoomOwner }: ButtonsProps) => {
+  const { leaveRoom, loading } = useLeaveRoom(room);
+
+  return (
+    <div className="mx-auto w-full max-w-xs">
+      {currentUserIsRoomOwner && (
+        <Button className="mb-4 w-full" disabled={loading} onClick={() => null}>
+          Update Room Info
+        </Button>
+      )}
+      <Button
+        className="w-full border-rose-500 font-bold text-rose-500 hover:bg-rose-500/10"
+        alt={true}
+        disabled={loading}
+        onClick={leaveRoom}
+      >
+        {loading ? "Leaving..." : "Leave Room"}
+      </Button>
+    </div>
+  );
+};
+
+export default Buttons;
