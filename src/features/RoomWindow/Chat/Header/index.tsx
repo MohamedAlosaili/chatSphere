9b03aa91197a4b8f48810661f0c915469010bb68
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-06-24 21:49:20 +0300

    Update: store current room info in useRef hook & Add RoomInfo toggler
    - Store room info in useRef to be to prevent null and empty values while
      closing the <Chat /> component when activeRoom is null
    - Add toggler to open RoomInfo

--- src/features/RoomWindow/Chat/Header/index.tsx
+++ src/features/RoomWindow/Chat/Header/index.tsx
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 import { HiDotsVertical } from "react-icons/hi";
 import { MdOutlineArrowBackIos } from "react-icons/md";
 
@@ -5,11 +6,19 @@ import Image from "@/components/Image";
 import { useRoomContext } from "@/context/RoomContext";
 import getRoomPhoto from "@/utils/getPhoto";
 
-// Types
-import { TRoom } from "@/types";
+interface HeaderProps {
+  toggleRoomInfo: () => void;
+}
 
-const Header = ({ room }: { room: TRoom }) => {
-  const { resetRoom } = useRoomContext();
+const Header = ({ toggleRoomInfo }: HeaderProps) => {
+  const { activeRoom, resetRoom } = useRoomContext();
+  /*
+   * The purpose of this useRef is to store the activeRoom info. Why?
+   * When activeRoom is reset the <Chat /> should be closed with a framer-motion
+   * animation, but at that time activeRoom will be null, so we need a variable to hold room
+   * info (persistent between renders) when <Chat /> is closing and activeRoom is null.
+   * */
+  const { current: room } = useRef(activeRoom!);
 
   return (
     <div className="sticky left-0 top-0 z-10 flex h-16 shrink-0 items-center gap-4 border-b border-accent/25 bg-bcolor-2/75 px-4 py-2">
@@ -25,7 +34,10 @@ const Header = ({ room }: { room: TRoom }) => {
         className="aspect-square rounded-xl"
       />
       <h3 className="font-semibold text-tcolor">{room.name}</h3>
-      <button className="ml-auto flex aspect-square w-8 items-center justify-center rounded-xl text-lg transition-colors hover:bg-bcolor/40">
+      <button
+        className="ml-auto flex aspect-square w-8 items-center justify-center rounded-xl text-lg transition-colors hover:bg-bcolor/40"
+        onClick={toggleRoomInfo}
+      >
         <HiDotsVertical />
       </button>
     </div>
